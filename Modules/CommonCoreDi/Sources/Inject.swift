dec602fdc72acefe9d80ff1e7fd77d057e012289
DoomPI <rslomtev@edu.hse.ru>
2024-05-17 02:47:44 +0300

    добавил Inject

---
+++ Modules/CommonCoreDi/Sources/Inject.swift
@@ -0,0 +1,22 @@
+import Swinject
+
+public class Inject {
+    
+    private init() { }
+    
+    static let shared = Inject()
+    
+    private var _container: Container? = nil
+    
+    public var container: Container {
+        get { return  _container! }
+    }
+    
+    func createDependencies(container: Container) {
+        _container = container
+    }
+    
+    func instance<T>() -> T {
+        return container.resolve(T.self)!
+    }
+}
